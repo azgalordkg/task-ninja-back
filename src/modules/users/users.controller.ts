12080f6795818336090a0badaf8255b3501b13f5
azamataltymyshev <aza.alt.com@gmail.com>
2023-05-09 19:07:52 +0300

    TNJ-64: add edit/delete methods

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,10 +1,22 @@
-import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpException,
+  Param,
+  Post,
+  Put,
+  UseGuards,
+} from '@nestjs/common';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { User } from './entities/user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
 import { RolesDecorator } from '../../decorators/role-auth.decorator';
 import { RolesAuthGuard } from '../auth/roles-auth.guard';
+import { UserDecorator } from '../../decorators/user.decorator';
+import { EditUserDto } from './dto/edit-user.dto';
 
 @ApiTags('Users')
 @Controller('users')
@@ -16,6 +28,33 @@ export class UsersController {
   @Post()
   create(@Body() dto: CreateUserDto) {
     return this.usersService.createUser(dto);
+  }
+
+  @ApiOperation({ summary: 'User editing' })
+  @ApiResponse({ status: 200, type: User })
+  @RolesDecorator('USER')
+  @UseGuards(RolesAuthGuard)
+  @Put()
+  edit(@Body() dto: EditUserDto, @UserDecorator() user: User) {
+    return this.usersService.editUser(dto, user.id);
+  }
+
+  @ApiOperation({ summary: 'User deletion' })
+  @ApiResponse({ status: 200, type: HttpException })
+  @RolesDecorator('USER')
+  @UseGuards(RolesAuthGuard)
+  @Delete()
+  delete(@UserDecorator() user: User) {
+    return this.usersService.deleteUser(user.id);
+  }
+
+  @ApiOperation({ summary: 'User deletion by Admin' })
+  @ApiResponse({ status: 200, type: HttpException })
+  @RolesDecorator('ADMIN')
+  @UseGuards(RolesAuthGuard)
+  @Delete('/:id')
+  deleteByAdmin(@Param('id') id: number) {
+    return this.usersService.deleteUser(id);
   }
 
   @ApiOperation({ summary: 'Users list' })
